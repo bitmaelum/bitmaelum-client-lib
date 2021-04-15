8603f0b33b1966e840e25d4b3318db9b005ca94e
Antonio Calatrava <antonio@antoniocalatrava.com>
2021-04-15 13:05:34 +0200

    new functions added

--- bridge/call.go
+++ bridge/call.go
@@ -3,6 +3,7 @@ package bitmaelumClientBridge
 import (
 	"encoding/json"
 	"sync"
+	"time"
 
 	bitmaelumClient "github.com/bitmaelum/bitmaelum-suite/library"
 )
@@ -22,9 +23,7 @@ func NewInstance() *instance {
 
 func GetInstance() *instance {
 	once.Do(func() {
-
 		clientInstance = bitmaelumClient.NewBitMaelumClient()
-
 	})
 
 	return &instance{client: clientInstance}
@@ -38,6 +37,8 @@ func Call(name string, payload []byte) ([]byte, error) {
 	switch name {
 	case "openVault":
 		output = instance.openVault(payload)
+	case "sendMessage":
+		output = instance.sendMessage(payload)
 	case "sendSimpleMessage":
 		output = instance.sendSimpleMessage(payload)
 	case "setClientFromVault":
@@ -46,9 +47,15 @@ func Call(name string, payload []byte) ([]byte, error) {
 		output = instance.setClientFromMnemonic(payload)
 	case "setClientFromPrivateKey":
 		output = instance.setClientFromPrivateKey(payload)
+	case "listMessages":
+		output = instance.listMessages(payload)
+	case "readBlock":
+		output = instance.readBlock(payload)
+	case "saveAttachment":
+		output = instance.saveAttachment(payload)
 	default:
 		return json.Marshal(map[string]interface{}{
-			"error":    "not implemented",
+			"error":    "the function " + name + " is not implemented on GO side",
 			"response": nil,
 		})
 	}
@@ -92,6 +99,35 @@ func (m instance) sendSimpleMessage(payload []byte) map[string]interface{} {
 	}
 
 	err = m.client.SendSimpleMessage(arguments["recipient"], arguments["subject"], arguments["body"])
+	if err != nil {
+		return map[string]interface{}{
+			"error": err.Error(),
+		}
+	}
+
+	return map[string]interface{}{
+		"error": nil,
+	}
+}
+
+func (m instance) sendMessage(payload []byte) map[string]interface{} {
+	type messageArguments struct {
+		Recipient   string            `json:"recipient"`
+		Subject     string            `json:"subject"`
+		Blocks      map[string]string `json:"blocks"`
+		Attachments []string          `json:"attachments"`
+	}
+
+	var arguments messageArguments
+
+	err := json.Unmarshal(payload, &arguments)
+	if err != nil {
+		return map[string]interface{}{
+			"error": "failed to decode arguments",
+		}
+	}
+
+	err = m.client.SendMessage(arguments.Recipient, arguments.Subject, arguments.Blocks, arguments.Attachments)
 	if err != nil {
 		return map[string]interface{}{
 			"error": err.Error(),
@@ -170,4 +206,97 @@ func (m instance) setClientFromPrivateKey(payload []byte) map[string]interface{}
 		"error": nil,
 	}
 
+}
+
+func (m instance) listMessages(payload []byte) map[string]interface{} {
+	type listArguments struct {
+		Since string `json:"since"`
+		Box   int    `json:"box"`
+	}
+
+	var arguments listArguments
+
+	err := json.Unmarshal(payload, &arguments)
+	if err != nil {
+		return map[string]interface{}{
+			"error": "failed to decode arguments",
+		}
+	}
+
+	since, err := time.Parse(time.RFC3339Nano, arguments.Since)
+	if err != nil {
+		return map[string]interface{}{
+			"error": err.Error(),
+		}
+	}
+
+	messages, err := m.client.ListMessages(since, arguments.Box)
+	if err != nil {
+		return map[string]interface{}{
+			"error": err.Error(),
+		}
+	}
+
+	return map[string]interface{}{
+		"response": messages,
+		"error":    nil,
+	}
+}
+
+func (m instance) readBlock(payload []byte) map[string]interface{} {
+	type listArguments struct {
+		MsgID   string `json:"msgid"`
+		BlockID string `json:"blockid"`
+	}
+
+	var arguments listArguments
+
+	err := json.Unmarshal(payload, &arguments)
+	if err != nil {
+		return map[string]interface{}{
+			"error": "failed to decode arguments",
+		}
+	}
+
+	block, err := m.client.ReadBlock(arguments.MsgID, arguments.BlockID)
+	if err != nil {
+		return map[string]interface{}{
+			"error": err.Error(),
+		}
+	}
+
+	return map[string]interface{}{
+		"response": block,
+		"error":    nil,
+	}
+}
+
+func (m instance) saveAttachment(payload []byte) map[string]interface{} {
+	type listArguments struct {
+		MsgID        string `json:"msgid"`
+		AttachmentID string `json:"attachmentid"`
+		Path         string `json:"path"`
+		Overwrite    bool   `json:"overwrite"`
+	}
+
+	var arguments listArguments
+
+	err := json.Unmarshal(payload, &arguments)
+	if err != nil {
+		return map[string]interface{}{
+			"error": "failed to decode arguments",
+		}
+	}
+
+	response, err := m.client.SaveAttachment(arguments.MsgID, arguments.AttachmentID, arguments.Path, arguments.Overwrite)
+	if err != nil {
+		return map[string]interface{}{
+			"error": err.Error(),
+		}
+	}
+
+	return map[string]interface{}{
+		"response": response,
+		"error":    nil,
+	}
 }
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/bitmaelum/bitmaelum-client-lib
 go 1.14
 
 require github.com/bitmaelum/bitmaelum-suite v0.1.2-0.20210407101626-d7630bb82a2b
+
+replace github.com/bitmaelum/bitmaelum-suite => /Users/acalatrava/Documents/Mis_Aplicaciones/GitHub/bitmaelum-suite
